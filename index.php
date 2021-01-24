878731fb0be598770e4fd7561c001113e641e4ae
Sebastianus Sembara <sembara9090@gmail.com>
2021-01-24 08:31:55 +0700

    progress bar view

--- index.php
+++ index.php
@@ -4,8 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
-    <h1>Hello World</h1>
+    <header>
+        <h1>Suhu dan Kelembapan Rumah Kaca</h1>
+    </header>
+
+    <div class="top-wrapper">
+        <h3>Suhu dan Kelembapan mempengaruhi pertumbuhan tanaman pada rumah kaca ini, pastikan suhu selalu direntang 25 - 30 derajat celcius dan kelembapan direntang nilai 60 - 70 persen</h3>
+
+        <h2>Suhu</h2>
+        <div class="progress" data-label="27'C">
+            <span class="value" style="width: 27%;"></span>
+        </div>
+
+        <h2>Kelembapan</h2>
+        <div class="progress" data-label="27'C">
+            <span class="value" style="width: 70%;"></span>
+        </div>
+
+    </div>
+
 </body>
 </html>
